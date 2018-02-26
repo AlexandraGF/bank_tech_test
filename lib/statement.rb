759c770e9b21f23c34bfdb5ef48623a63e7b3c35
Alexy <alexandra.fina@yahoo.com>
2018-02-26 14:56:52 +0000

    Make first test pass(deposit method)

---
+++ lib/statement.rb
@@ -0,0 +1,13 @@
+class Statement
+
+  def initialize(date, total = 0, balance = 0)
+    @date = date
+    @total = total
+    @balance = balance
+  end
+
+  def deposit(amount)
+
+  end
+
+end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,6 +1,8 @@
+require './lib/statement.rb'
+
 describe Statement do
   it 'client can make a deposit' do
-    statement = Statement.new
+    statement = Statement.new('14/01/2018')
     statement.deposit(2000)
     expect(statement).to respond_to(:deposit).with(1).argument
   end
