aa733b3aa5ae3a91340df380b9be868b2d213a76
Alexy <alexandra.fina@yahoo.com>
2018-02-26 17:31:32 +0000

    Refactor tests

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,39 +1,36 @@
 require './lib/statement.rb'
 
 describe Statement do
+
+  let(:statement) {Statement.new}
+
   it 'client can make a deposit' do
-    statement = Statement.new
     statement.deposit('14/01/2018', 2000)
     expect(statement).to respond_to(:deposit).with(2).arguments
   end
 
   it 'statement has the deposit in the account' do
-    statement = Statement.new
     statement.deposit('14/01/2018', 2000)
     expect(statement.balance).to eq(2000)
   end
 
   it 'after withdrawal, new balance should be lower' do
-    statement = Statement.new
     statement.deposit('14/01/2018', 2000)
-    withdrawal = statement.withdrawal('14/01/2018', 500)
+    statement.withdrawal('14/01/2018', 500)
     expect(statement.balance).to eq(1500)
   end
 
   it 'after deposit, checks the account storage has the new transaction with date, amount, balance' do
-    statement = Statement.new
     statement.deposit('14/01/2018', 2000)
     expect(statement.storage.length).to eq(1)
   end
 
   it 'account statement can be printed on screen with date, deposit/withdrawal and new balance' do
-    statement = Statement.new
     statement.deposit("14/01/2018", 2000)
     expect(statement.storage[0]).to eq("14/01/2018 || 2000.00 || || 2000.00")
   end
 
   it 'after withdrawal, checks the account storage has the new transaction with date, amount, balance' do
-    statement = Statement.new
     statement.deposit('14/01/2018', 2000)
     statement.withdrawal('14/01/2018', 1000)
     expect(statement.storage.length).to eq(2)
