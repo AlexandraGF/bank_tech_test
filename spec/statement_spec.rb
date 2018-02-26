f644abbd848313ea3f07a9bbcb74d5966e0d6905
Alexy <alexandra.fina@yahoo.com>
2018-02-26 15:24:06 +0000

    Test for show statement on screen

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -20,4 +20,13 @@ describe Statement do
     expect(statement.balance).to eq(1500)
   end
 
+  it 'account statement can be printed on screen with date, deposit/withdrawal and new balance' do
+    statement = Statement.new('14/01/2018')
+    statement.deposit(2000)
+    withdrawal = statement.withdrawal(500)
+    statement.deposit(300)
+    withdrawal = statement.withdrawal(100)
+    expect(statement.show).to eq("14/01/2018 || || 100.00 || 1700.00")
+  end
+
 end
