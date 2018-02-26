6e9e48d38d36a147518b849667b28f30ce76533d
Alexy <alexandra.fina@yahoo.com>
2018-02-26 14:59:20 +0000

    Test for expect new balance to reflect the deposit

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -6,4 +6,10 @@ describe Statement do
     statement.deposit(2000)
     expect(statement).to respond_to(:deposit).with(1).argument
   end
+
+  it 'statement has the deposit in the account' do
+    statement = Statement.new('14/01/2018')
+    total = statement.deposit(2000)
+    expect(total).to eq(2000)
+  end
 end
